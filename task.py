7ef628adfcef44fd979d505cb29399ae900e5e03
Masha1111 <sowix@yandex.ru>
2018-12-13 23:50:21 +0500

    build matrix

--- task.py
+++ task.py
@@ -1,13 +1,19 @@
 def get_field_config(file):
     data = file.replace(' ', '').replace('\n', '')
-    array = [[0 for i in range(8)] for j in range(8)]
+    array = [[0 for i in range(10)] for j in range(10)]
     letter = 'W'
     for i in range(0, len(data) - 2, 2):
         if int(data[i]) != 0:
-            array[int(data[i + 1]) - 1][int(data[i]) - 1] = letter
+            array[int(data[i + 1])][int(data[i])] = letter
         else:
             letter = 'B'
             continue
+    for i in [0, 9]:
+        for j in range(10):
+            array[i][j] = 'WB'
+    for i in range(10):
+        for j in [0, 9]:
+            array[i][j] = 'WB'
     return array
 
 
